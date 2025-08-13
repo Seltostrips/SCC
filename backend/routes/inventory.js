058f1c6eb4f865df2918e0d2eeffae16a021c50f
Seltostrips <seltostrips@gmail.com>
2025-08-14 01:19:45 +0530

    Update inventory.js

--- backend/routes/inventory.js
+++ backend/routes/inventory.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router');
+const router = express.Router();
 const auth = require('../middleware/auth');
 const Inventory = require('../models/Inventory');
 const User = require('../models/User');
